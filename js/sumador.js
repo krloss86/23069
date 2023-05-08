99060b4a26fd3f47f570ec5b6bf587be82ff8ed5
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2023-05-08 19:39:27 -0300

    add js function,event listener

--- js/sumador.js
+++ js/sumador.js
@@ -1,15 +1,36 @@
-// alert('hola mundo!!!');
-//esto es un comentario!!! de una sola linea
+//asi se crea una funcion
+//no tiene parametros de entrada
+function sumar() {
+    
+    //obtener la referencia al input con id="valor1"
+    //usando el D.O.M
+    const apuntadorAValor1 = document.getElementById('valor1');
+    const apuntadorAValor2 = document.getElementById('valor2');
 
-/*
- es 
- un comentario
- de 
- varias 
- lineas
-*/
+    //ahora obtengo el valor de cada apuntados
+    //y lo guarda en una  variable
+    const v1 = toNumber(apuntadorAValor1.value);
+    const v2 = toNumber(apuntadorAValor2.value);
 
-const valor1 = parseInt(prompt('Ingrese valor1'));
-const valor2 = 20;
-const resultado = valor1 + valor2;
-console.log(resultado);
+    //update del div con la suma de v1+v2
+    //o sea calculo la suma +
+    const suma = v1 + v2;
+
+    const apuntadorAlResultado = document.getElementById('resultado');
+    //en los div no existe value (como el input) existe innerHTML
+    apuntadorAlResultado.innerHTML = suma;
+}
+function sumarv2() {
+    const suma = toNumber(document.getElementById('valor1').value) + toNumber(document.getElementById('valor2').value);
+    document.getElementById('resultado').innerHTML = suma;
+}
+function toNumber (valor) {
+    //lógica de la función
+    const valorNumerico = Number(valor);
+    return valorNumerico;
+ }
+
+ //asigno un evento via javascript
+ const apuntadorAlBotonSuma2 = document.getElementById('btnSumador');
+ //agrego un evento=click y asociao una funcion
+ apuntadorAlBotonSuma2.addEventListener('click', sumarv2);
